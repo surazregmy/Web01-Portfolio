47faa75a9ce2374bda0b2b253dd4f3411b34b61f
surazregmy <suraz.regmy@gmail.com>
2022-02-05 23:13:27 -0500

    Form created and redirect to homepage when submited

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,26 @@ router.get("/about", function (req, res, next) {
   res.render("about", { title: "About Me" });
 });
 
+/* GET Projects page. */
+router.get("/projects", function (req, res, next) {
+  res.render("projects", { title: "Projects" });
+});
+
+/* GET services page. */
+router.get("/services", function (req, res, next) {
+  res.render("services", { title: "Services" });
+});
+
+/* GET services page. */
+router.get("/contactme", function (req, res, next) {
+  res.render("contactme", { title: "Contact Me" });
+});
+
+/* save info for contact form. */
+router.get("/savecontact", function (req, res, next) {
+  //display form value
+  res.render("thanks.ejs", { body: req.body });
+  return res.redirect("/");
+});
+
 module.exports = router;
