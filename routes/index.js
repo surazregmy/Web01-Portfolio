f3ddb5e4d3d3799dd27f9524f8c12cdaef9a0cf9
surazregmy <suraz.regmy@gmail.com>
2022-02-05 22:33:38 -0500

    static pages made and about me page updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Welcome to Suraj Regmi's Profile" });
+});
+
+/* GET About page. */
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About Me" });
 });
 
 module.exports = router;
